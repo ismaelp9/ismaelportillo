360c25bddad5a2d76f0fc06869a90361993304d4
Ivan <talaveronromeroivan@gmail.com>
2019-10-14 10:33:34 +0200

    modificacion de aula

--- update.sql
+++ update.sql
@@ -1,3 +1,3 @@
-update usuarios set Edad="21" where Edad="44";
-
-update cursos set nombre="Redes" where nombre="ASIR";
+update usuarios set Edad=44 where Edad=21;
+update cursos set nombre='Redes' where nombre='ASIR';
+update aula set Capacidad=4 where Capacidad=5;
