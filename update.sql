2aacb0340c109f533e19fd61dfa5f8fd31805822
Guillermo Jose <guilleparadas.7@gmail.com>
2019-10-16 11:48:15 +0200

    añado_edificio

--- update.sql
+++ update.sql
@@ -1,3 +1,4 @@
 update usuarios set Edad=44 where Edad=21;
 update cursos set nombre='Redes' where nombre='ASIR';
 update aula set Capacidad=4 where Capacidad=5;
+update edificio set nombre="Info" where descripcion="Reforma";
